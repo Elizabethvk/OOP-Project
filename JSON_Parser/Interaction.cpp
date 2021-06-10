08276ef7c3ad4f49ee9d344930440829db04871a
elizabethvk <elizabeetvk@gmail.com>
2021-06-11 02:19:53 +0300

    Interaction

---
+++ JSON_Parser/Interaction.cpp
@@ -0,0 +1,9 @@
+#include "Interaction.hpp"
+
+#include "Commands/CommandHelp.hpp"
+#include "Commands/CommandFileOpen.hpp"
+#include "Commands/CommandValidate.hpp"
+#include "Commands/CommandSearch.hpp"
+#include "Commands/CommandPrint.hpp"
+#include "Commands/CommandClose.hpp"
+
--- JSON_Parser/Interaction.hpp
+++ JSON_Parser/Interaction.hpp
@@ -1,9 +1,23 @@
 #ifndef INTERACTION_H
 #define INTERACTION_H
 
+#include <memory>
+
+#include "Commands/ICommand.hpp"
+#include "JSONCheck/JSONCheck.hpp"
 
 class Interaction {
+private:
+    JsonCheck jsonChecker;
 
+    vector<std::unique_ptr<ICommand()>> userCommands;
+
+    void getCommand(const string& command);
+    // void applyArguments(const string& commandName, vector<string> arguments);
+
+public:
+    Interaction();
+    void run();
 };
 
 
