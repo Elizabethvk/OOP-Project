77c8ab450a7d47f9bb086d1a692017c6fcdd67cf
elizabethvk <elizabeetvk@gmail.com>
2021-06-11 23:42:50 +0300

    main

--- JSON_Parser/main.cpp
+++ JSON_Parser/main.cpp
@@ -1,41 +1,10 @@
+#include "Interaction.hpp"
 #include "JSONCheck/JSONCheck.hpp"
 
 int main() {
-    JsonCheck testClass;
-    // string testStr = "{\"menu\": {\
-    //                     \"id\": \"file\",\
-    //                     \"value\": \"File\",\
-    //                     \"popup\": {\
-    //                         \"menuitem\": [\
-    //                         {\"value\": \"New\", \"onclick\": \"CreateNewDoc()\"},\
-    //                         {\"value\": \"Open\", \"onclick\": \"OpenDoc()\"},\
-    //                         {\"value\": \"Close\", \"onclick\": \"CloseDoc()\"}\
-    //                         ]\
-    //                     }\
-    //                     }}";
+    JsonCheck manager;
+    Interaction starter;
+    starter.start();
 
-    string testStr = "{\n"
-                        "    \"id\": \"okdok\",\n"
-                        "    \"name\": \"Foo\",\n"
-                        "    \"tags\": [ \"Fighters\", \"Five\" ],\n"
-                        "    \"type\": {\n"
-                        "        \"rock\": \"100\",\n"
-                        "        \"roll\": \"30\"\n"
-                        "    }\n"
-                        "}";
-
-    stringstream ss;
-
-    ss << testStr;
-
-    try
-    {
-        testClass.inputJson(ss)->print();
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
-    
     return 0;
 }
